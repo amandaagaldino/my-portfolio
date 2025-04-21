774efd35e47b2b2a8ed87abac13fda926cf5c396
amandaagaldino <amandagaldinox@gmail.com>
2025-04-20 22:15:19 -0300

    detalhes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import Footer from "./components/Footer/Footer"
+import NavBar from "./components/NavBar/NavBar"
+import AboutSection from "./pages/Home/sections/AboutSection/AboutSection"
+import HeroSection from "./pages/Home/sections/HeroSection/HeroSection"
+import ProjectsSection from "./pages/Home/sections/ProjectsSection/ProjectsSection"
 
 
 const App = () => {
 
   return (
     <>
-      App
+      <NavBar/> 
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection/>
+      <Footer />
     </>
   )
 }
