474d2957b2614fab00f75ad301b061214ac4ece1
amandaagaldino <amandagaldinox@gmail.com>
2025-04-20 21:46:45 -0300

    atualizando

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -1,35 +1,38 @@
-import { styled } from "@mui/material";
-import { ReactNode } from "react";
 
-interface StyledButtonProps{
-    children: ReactNode
+import React, { ReactNode } from "react";
+import { styled } from "@mui/material";
+
+interface StyledButtonProps {
+    children: ReactNode;
+    onClick?: () => void;
 }
 
-const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
+const StyledButton: React.FC<StyledButtonProps> = ({ children, onClick }) => {
 
-    const StyledButton = styled("button")(({theme}) => ({
+    const StyledButton = styled("button")(({ theme }) => ({
         backgroundColor: "transparent",
-        border: `1px solid ${theme.palette.primary.contrastText}` ,
-        borderRadius: "3px", 
-        padding: "5px 15px",
-        width: "100%",
-        color: theme.palette.primary.contrastText,
         display: "inline-flex",
         alignItems: "center",
         justifyContent: "center",
+        verticalAlign: "middle",
         gap: "10px",
-        '&:hover':{
-            backgroundColor: theme.palette.secondary.light
+        borderRadius: "3px",
+        padding: "5px 15px",
+        width: "100%",
+        border: `1px solid  ${theme.palette.primary.contrastText}`,
+        color: theme.palette.primary.contrastText,
+        cursor: "pointer",
+        transition: "background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.light,
         }
-    }))
+    }));
 
-    return(
+    return (
         <>
-            <StyledButton>
-                {children}
-            </StyledButton>
+            <StyledButton onClick={onClick}>{children}</StyledButton>
         </>
     )
 }
 
-export default StyledButton;
+export default StyledButton
--- src/theme.ts
+++ src/theme.ts
@@ -1,19 +1,20 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 let theme = createTheme({
-    palette: {
+  typography: {
+      fontFamily: [
+          '"Helvetica Neue"',
+      ].join(','),
+  },
+  palette: {
       primary: {
-        main: '#232323',
+          main: '#232323',
       },
       secondary: {
-        main: '#4f8e3e',
+          main: '#4f8e3e',
       },
-    },
-    typography: {
-        fontFamily: "Helvetica Neue"
-    }
-  });
+  },
+});
+theme = responsiveFontSizes(theme);
 
-  theme = responsiveFontSizes(theme);
-
-  export default theme;
+export default theme;
